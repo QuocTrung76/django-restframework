92ea771552a826a20a628a3361ce095707656b15
Home <you@example.com>
2022-04-08 00:08:42 +0700

    modified restframework

---
+++ MyProject/api_basic/serializers.py
@@ -0,0 +1,9 @@
+import imp
+from yaml import serialize
+from rest_framework import serializers
+from .models import Article
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'author']
